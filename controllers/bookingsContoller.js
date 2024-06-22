9344c6884f694d1d18e4924d5fec4a1db1ce89f3
Lathika Sunder <lathikasunder11@gmail.com>
2024-06-22 16:30:59 +0530

    Add - Calculated Booking Cost

--- controllers/bookingsContoller.js
+++ controllers/bookingsContoller.js
@@ -1,11 +1,12 @@
 const Booking = require("../models/bookingModel");
-const cron=require('node-cron')
+const cron = require('node-cron');
+const Bus = require("../models/busModel");
 
 
 
 
 const postBooking = async (request, response) => {
-    const userId=request.user._id;
+    const userId = request.user._id;
     const {
         boardingPoint,
         endingPoint,
@@ -14,7 +15,7 @@ const postBooking = async (request, response) => {
         passengers,
         paymentStatus,
         busId,
-       } = request.body;
+    } = request.body;
 
     const booking = new Booking({
         boardingPoint,
@@ -24,38 +25,74 @@ const postBooking = async (request, response) => {
         passengers,
         paymentStatus,
         busId,
-        userId:userId,
+        userId: userId,
     })
 
     try {
         await booking.save()
-        response.status(201).json({message:"Booking Successful",booking:booking})
+        response.status(201).json({ message: "Booking Successful", booking: booking })
     } catch (error) {
-        response.status(500).json({message:"Internal Server Error",error:error})
+        response.status(500).json({ message: "Internal Server Error", error: error })
     }
 }
 
-const getBookingsOfUser=async(request,response)=>{
-    const userId=request.user._id
+const getBookingsOfUser = async (request, response) => {
+    const userId = request.user._id
     try {
-        const bookingsOfUser=await Booking.find({userId:userId})
+        const bookingsOfUser = await Booking.find({ userId: userId })
         response.status(200).json(bookingsOfUser)
     } catch (error) {
-        response.status(500).json({message:"Internal Server Error",error:error.message})
+        response.status(500).json({ message: "Internal Server Error", error: error.message })
     }
 }
 
-const getBookingDetails=async(request,response)=>{
-    const bookingId=request.params.bookingId
+const getBookingDetails = async (request, response) => {
+    const bookingId = request.params.bookingId
     try {
-        const booking=await Booking.findById(bookingId)
+        const booking = await Booking.findById(bookingId)
         if (!booking) {
             return response.status(404).json({ message: "Booking not found" });
         }
         response.status(200).json(booking)
     } catch (error) {
-        response.status(500).json({message:"Internal Server Error",error:error.message})
-        
+        response.status(500).json({ message: "Internal Server Error", error: error.message })
+
     }
 }
-module.exports = {postBooking,getBookingsOfUser,getBookingDetails}
+
+const calculateBookingCost = async (request, response) => {
+    const { busId, seats } = request.body;
+
+    try {
+        const bus = await Bus.findById(busId);
+
+        if (!bus) {
+            return response.status(404).json({ message: "Bus not found" });
+        }
+
+        const busPrice = bus.price;
+        let totalBusPrice = 0;
+
+        const seatCategories = {
+            "L1": 3.5, "L2": 3.5, "L6": 3.5, "L7": 3.5, "U1": 3.5, "U2": 3.5, "U6": 3.5, "U7": 3.5,
+            "L3": 7.5, "L4": 7.5, "L8": 7.5, "L9": 7.5, "U3": 7.5, "U4": 7.5, "U8": 7.5, "U9": 7.5,
+            "L5": 10.5, "L10": 10.5, "U5": 10.5, "U10": 10.5
+        };
+
+        for (let i = 0; i < seats.length; i++) {
+            const seatCategory = seats[i];
+
+            if (seatCategories[seatCategory]) {
+                totalBusPrice += busPrice * seatCategories[seatCategory];
+            } else {
+                return response.status(400).json({ message: `Invalid seat category: ${seatCategory}` });
+            }
+        }
+
+        return response.status(200).json(totalBusPrice );
+    } catch (error) {
+        response.status(500).json({ message: "Internal Server Error", error: error.message });
+    }
+};
+
+module.exports = { postBooking, getBookingsOfUser, getBookingDetails, calculateBookingCost }
--- routes/bookingsRouter.js
+++ routes/bookingsRouter.js
@@ -1,14 +1,13 @@
 const express=require('express')
-const { postBooking,getBookingsOfUser,getBookingDetails } = require('../controllers/bookingsContoller')
+const { postBooking,getBookingsOfUser,getBookingDetails, calculateBookingCost } = require('../controllers/bookingsContoller')
 const verifyToken = require('../middleware/verifyToken')
 const router=express.Router()
 
 router.get('/getBookingsOfUser',verifyToken,getBookingsOfUser)
 router.get('/getBookingDetails/:bookingId',getBookingDetails)
-router.get('/getBookingsOfBus/:busId')
 router.post('/postBooking',verifyToken,postBooking)
-router.get('/calculateBookingCost')
-router.delete('/cancelBoking')
+router.get('/calculateBookingCost',calculateBookingCost)
+router.delete('/cancelBooking')
 router.put('/updateBooking')
 
 module.exports=router
